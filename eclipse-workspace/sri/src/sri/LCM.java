2532313000d23ce23397acb8e0fd43a9d3f94ae1
Srisowmya <112609839+srisowmya1212@users.noreply.github.com>
2024-08-10 18:10:36 +0530

    Update LCM.java

--- eclipse-workspace/sri/src/sri/LCM.java
+++ eclipse-workspace/sri/src/sri/LCM.java
@@ -47,3 +47,57 @@ public class LCM {
 	}
 
 }
+
+//refer fro calcualating gcd
+
+package com.sri.practice;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the size of an array:  ");
+		int size = sc.nextInt();
+		int[] obj = new int[size];
+		for (int i = 0; i < size; i++) {
+			obj[i] = sc.nextInt();
+		}
+		System.out.println("the elements of an array is: ");
+		System.out.print("[");
+		for (int a : obj) {
+			System.out.print(a + " ");
+		}
+		System.out.println("]");
+		int lcm = gcd(obj);
+		System.out.println(lcm);
+
+	}
+
+	public static int gcd(int[] obj) {
+		int gcd = obj[0];
+		for (int i = 1; i < obj.length; i++) {
+			gcd = lcmgcd(gcd, obj[i]);
+		}
+		return gcd;
+	}
+
+	public static int lcmgcd(int a, int b) {
+		int gcd=1;
+		int min = (a > b) ? b : a;// without this we cant get gcd
+		for (int i = min; i >= 1; i--) {// for gcd it is min and for lcm it is max
+			if (a % i == 0 && b % i == 0) {
+				
+				gcd=i;
+
+				return gcd;
+			}
+		}
+		
+		
+		return 1;
+	}
+}
+
