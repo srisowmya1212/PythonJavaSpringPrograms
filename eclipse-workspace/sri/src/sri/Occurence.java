73a2245fb77b719f35c8f108619b67dd8f391373
Srisowmya <112609839+srisowmya1212@users.noreply.github.com>
2024-08-12 11:45:57 +0530

    Update Occurence.java

--- eclipse-workspace/sri/src/sri/Occurence.java
+++ eclipse-workspace/sri/src/sri/Occurence.java
@@ -1,3 +1,5 @@
+//refere below progaran
+
 package sri;
 
 import java.util.Scanner;
@@ -49,3 +51,75 @@ public class Occurence {
 	}
 
 }
+
+///refer this
+
+
+package com.sri.practice;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of array");
+		int size=sc.nextInt();
+		System.out.println("Enter the array of elements");
+		int[] obj=new int[size];
+		int[] occ=new int[size];
+		for(int i=0;i<size;i++) {
+			obj[i]=sc.nextInt();
+		}
+		for(int a:obj) {
+			System.out.print(a+" ");
+		}
+		
+		for(int i=0;i<obj.length;i++) {
+			int cunt=1;
+			for(int j=i+1;j<obj.length;j++) {
+				if(obj[i]==obj[j]) {
+					occ[j]=1;
+					cunt++;
+				}
+			}
+			if(occ[i]!=1) {
+				occ[i]=cunt;
+			}
+		}
+		System.out.println("The occcurence of elemenet");
+		for(int i=0;i<occ.length;i++) {
+			if(occ[i]!=1) {
+				System.out.println("the occcurence of element " + obj[i] + "  is " + occ[i]);
+			}
+		}
+		
+	}
+}
+o/p:
+
+5
+Enter the array of elements
+2
+3
+2
+3
+2
+2 3 2 3 2 The occcurence of elemenet
+the occcurence of element 2  is 3
+the occcurence of element 3  is 2
+
+	o/p2
+
+Enter the size of array
+5
+Enter the array of elements
+1
+2
+3
+2
+3
+1 2 3 2 3 The occcurence of elemenet
+the occcurence of element 2  is 2
+the occcurence of element 3  is 2
